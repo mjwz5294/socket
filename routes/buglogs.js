ecd205a0ffbec4e3d7848a5dcbc390d8b41de787
“mjwz5294” <393578079@qq.com>
2018-09-20 14:50:27 +0800

    删除bug后回调到正确页面

--- routes/buglogs.js
+++ routes/buglogs.js
@@ -1,8 +1,8 @@
-var UsersModels = require('../models/users'); //数据操作对象
+
 module.exports = {
     router: function(req, res, next) {
         mgrMySQL.query('select * from buglogs', null, function (error, results, fields) {
-            console.log(JSON.stringify(results));
+            // console.log(JSON.stringify(results));
             res.render('buglogs', {
                 title: '欢迎客官大人',
                 buglog: results,
@@ -31,13 +31,13 @@ module.exports = {
     },
     delete:function(req, res, next){
         var id = req.params.id;
-        console.log(JSON.stringify(req.params));
+        // console.log(JSON.stringify(req.params));
         if (id) {
 
             mgrMySQL.query('DELETE FROM buglogs where bug_id=?', parseInt(id), function (error, results, fields) {
                 if (!error){
                     console.log('数据删除成功')
-                    return res.redirect('/users.html')
+                    return res.redirect('/buglogs.html')
                 }else {
                     console.log(error)
                 }
